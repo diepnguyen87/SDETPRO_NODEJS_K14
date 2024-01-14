c642ab3f9583fdcfd6ccb777f84de1f8add653e9
Diep Nguyen <diepniit.nguyenhong@gmail.com>
2024-01-14 17:01:04 +0700

    Init new project with PlayWright

---
+++ lessons/lesson_1/HelloWorld.js
@@ -0,0 +1,1 @@
+console.log("Welcome to Automation with Playwright");
---
+++ lessons/lesson_1/VariableLearning.js
@@ -0,0 +1,19 @@
+//declare variable
+let total = 1
+total = true
+
+//declare constant number
+const PI = 3.141
+//Not allow reassign: PI = 1 
+
+//Manipulate with number
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.MAX_VALUE + 1);
+console.log(Infinity > (Number.MAX_VALUE + 1));
+console.log(Infinity > 1.7976931348623157e+309);
+
+//Type of
+console.log(typeof PI);
+console.log(typeof total);
+
